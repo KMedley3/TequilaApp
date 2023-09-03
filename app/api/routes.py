802c6402c3391cfea2ad51eddbc18b3597afd84f
Keenan <KMedley3@gmail.com>
2023-09-03 17:41:56 -0400

    api update routes

--- app/api/routes.py
+++ app/api/routes.py
@@ -8,21 +8,52 @@ api = Blueprint('api',__name__, url_prefix='/api')
 def getdata():
     return {'yee': 'haw'}
 
-@api.route('/drink', methods = ['POST'])
+@api.route('/tequilaprofile', methods = ['POST'])
 @token_required
 def create_drink(current_user_token):
     name = request.json['name']
-    alc_percentage = request.json['alcohol percentage']
-    tequila_type = request.json['tequila type']
+    alc_percentage = request.json['alc_percentage']
+    tequila_type = request.json['tequila_type']
     origin = request.json['origin']
     user_token = current_user_token.token
 
     print(f'BIG TESTER: {current_user_token.token}')
 
-    drink = Drink(name, alc_percentage, tequila_type, origin, user_token = user_token )
+    drink = Drink(name, alc_percentage, origin, tequila_type, user_token = user_token )
 
     db.session.add(drink)
     db.session.commit()
 
-    response = Drinks_schema.dump(drink)
+    response = Drink_schema.dump(drink)
+    return jsonify(response)
+
+@api.route('/tequilaprofile', methods = ['GET'])
+@token_required
+def get_drink(current_user_token):
+    a_user = current_user_token.token
+    new_drink = Drink.query.filter_by(user_token = a_user).all()
+    response = Drinks_schema.dump(new_drink)
+    return jsonify(response)
+
+@api.route('/tequilaprofile/<id>', methods = ['POST','PUT'])
+@token_required
+def update_drink(current_user_token,id):
+    drink = Drink.query.get(id) 
+    drink.name = request.json['name']
+    drink.alc_percentage = request.json['alc_percentage']
+    drink.origin = request.json['origin']
+    drink.tequila_type = request.json['tequila_type']
+    drink.user_token = current_user_token.token
+
+    db.session.commit()
+    response = Drink_schema.dump(drink)
+    return jsonify(response)
+
+@api.route('/tequilaprofile/<id>', methods = ['DELETE'])
+@token_required
+def delete_car(current_user_token, id):
+    drink = Drink.query.get(id)
+    db.session.delete(drink)
+    db.session.commit()
+    response = Drink_schema.dump(drink)
     return jsonify(response)
